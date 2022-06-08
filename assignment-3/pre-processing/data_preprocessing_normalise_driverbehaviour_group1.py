8654592fc13e66ea746aeb3a2ef2e0164dc0ed1a
minshiwu-hub <91583537+minshiwu-hub@users.noreply.github.com>
2022-06-08 18:28:14 +0930

    Add files via upload

---
+++ assignment-3/pre-processing/data_preprocessing_normalise_driverbehaviour_group1.py
@@ -0,0 +1,32 @@
+# Import libraries
+from pandas import read_csv
+from numpy import set_printoptions
+from sklearn.preprocessing import Normalizer
+
+# Load the csv file using read_csv function of pandas library
+myFilename = 'TrainingDriverBehaviourInliersData.csv'
+
+# Define the data variable names
+myNames = ['Experience', 'KlmsDist', 'Meter', 'Occupants', 'VehSeq', 'Airbag', 'AlcoholRel', 'LicClass', 'PedVMC',
+           'HeavyVeh', 'Carriage', 'DUI', '4WD', 'Intername', 'LicStatus', 'NTRes', 'Community', 'Country', 'SafDevice',
+           'Sex', 'State1', 'RegoState', 'RoadDivision', 'RoadName', 'RoadUser1', 'RoadWidth', 'Rural', 'SpeedRel',
+           'SurfaceType', 'TSD', 'UnitType', 'VehMov', 'VehVMC', 'VertFeature', 'Weather', 'CyclistInv', 'ArticVeh1',
+           'RigidVeh', 'ArticVeh2', 'ContFactor']
+
+# Read dataset
+myDataframe = read_csv(myFilename, names=myNames)
+
+# Transform data set into an array
+myArray = myDataframe.values
+
+# Split the array to input (39 explanatory variables) and output (1 response variable)
+myExplanatoryvariables = myArray[:,0:39]
+myResponsivevariable = myArray[:,39]
+
+myScaler = Normalizer().fit(myExplanatoryvariables)
+myNormalisedata = myScaler.fit_transform(myExplanatoryvariables)
+
+set_printoptions(precision=3)
+print()
+print(myNormalisedata[0:5,:])
+print()
---
+++ assignment-3/pre-processing/data_preprocessing_strandardise_roadcrash_group1.py
@@ -0,0 +1,32 @@
+# Import libraries
+from pandas import read_csv
+from numpy import set_printoptions
+from sklearn.preprocessing import StandardScaler
+
+# Load the csv file using read_csv function of pandas library
+myFilename = 'TrainingRoadCrashInliersData.csv'
+
+# Define the data variable names
+myNames = ['Mcycle', 'NbrTow', 'Airbag', 'VehVMC1', 'LicStatus', 'ABSSA3', 'Aboriginal', 'AgeBand', 'SafDevice',
+           'Sex', 'State1', 'State2', 'RoadUser1', 'RoadWidth', 'TrafDensity', 'ATVInv', 'UnitType', 'VehDirTravel',
+           'VehMov', 'VertFeature', 'RigidVeh', 'Businv', 'Pedestrian', 'InjuryDesc']
+
+# Read dataset
+myDataframe = read_csv(myFilename, names=myNames)
+
+# Transform data set into an array
+myArray = myDataframe.values
+
+# Split the array to input (23 explanatory variables) and output (1 response variable)
+myExplanatoryvariables = myArray[:,0:23]
+myResponsivevariable = myArray[:,23]
+
+# Apply standardisation algorithm
+myScaler = StandardScaler().fit(myExplanatoryvariables)
+myStandardiseddata = myScaler.fit_transform(myExplanatoryvariables)
+
+# Print results
+set_printoptions(precision=3)
+print()
+print(myStandardiseddata[0:5,:])
+print()
